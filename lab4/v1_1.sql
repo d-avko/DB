ea5e46f4a616c9baca86cf59950f1b0a523b9596
Denis Avko <actualdennis@gmail.com>
2020-10-21 18:34:39 +0300

    4

---
+++ lab4/v1_1.sql
@@ -0,0 +1,85 @@
+﻿USE AdventureWorks2012;
+GO
+
+/*
+ Создайте таблицу Production.ProductCategoryHst, 
+ которая будет хранить информацию об изменениях в таблице Production.ProductCategory.
+*/
+
+create table Production.ProductCategoryHst(
+	ID INT IDENTITY(1, 1) PRIMARY KEY,
+	Action CHAR(6) NOT NULL CHECK (Action IN('INSERT', 'UPDATE', 'DELETE')),
+	ModifiedDate DATETIME NOT NULL DEFAULT GETDATE(),
+	SourceID INT NOT NULL,
+	UserName VARCHAR(50) NOT NULL
+);
+
+GO
+/*
+
+Создайте один AFTER триггер для трех операций INSERT, UPDATE, DELETE для таблицы Production.ProductCategory. 
+Триггер должен заполнять таблицу Production.ProductCategoryHst с указанием типа операции 
+в поле Action в зависимости от оператора, вызвавшего триггер.
+*/
+
+CREATE TRIGGER Production.ProductCategory_Insert_Update_Delete
+ON Production.ProductCategory
+AFTER UPDATE, INSERT, DELETE AS
+	if exists(SELECT * from inserted) and exists (SELECT * from deleted)
+	begin
+		INSERT INTO Production.ProductCategoryHst(Action, ModifiedDate, SourceID, UserName)
+		SELECT 'UPDATE', GETDATE(), upd.ProductCategoryID, USER_NAME()
+		FROM inserted AS upd;
+	end
+
+	If exists (Select * from inserted) and not exists(Select * from deleted)
+	begin
+		INSERT INTO Production.ProductCategoryHst(Action, ModifiedDate, SourceID, UserName)
+		SELECT 'INSERT', GETDATE(), ins.ProductCategoryID, USER_NAME()
+		FROM inserted AS ins;
+	end
+
+	If exists(select * from deleted) and not exists(Select * from inserted)
+	begin 
+		INSERT INTO Production.ProductCategoryHst(Action, ModifiedDate, SourceID, UserName)
+		SELECT 'DELETE', GETDATE(), del.ProductCategoryID, USER_NAME()
+		FROM deleted AS del;
+	end
+
+GO
+
+/*
+Создайте представление VIEW, отображающее все поля таблицы Production.ProductCategory.
+*/
+
+CREATE VIEW Production.vProductCategory
+WITH ENCRYPTION
+AS 
+	SELECT * FROM Production.ProductCategory;
+GO
+
+/*
+Вставьте новую строку в Production.ProductCategory через представление. 
+Обновите вставленную строку. 
+Удалите вставленную строку. 
+Убедитесь, что все три операции отображены в Production.ProductCategoryHst.
+*/
+
+INSERT INTO Production.vProductCategory (
+	Name, 
+	rowguid, 
+	ModifiedDate)
+VALUES ('Electronic devices', NEWID(), GetDate());
+
+GO
+
+UPDATE Production.vProductCategory SET Name = 'Mobile devices' WHERE Name = 'Electronic devices';
+
+GO
+
+DELETE Production.vProductCategory WHERE Name = 'Mobile devices';
+
+GO
+
+SELECT * 
+FROM Production.ProductCategoryHst;
---
+++ lab4/v1_2.sql
@@ -0,0 +1,128 @@
+﻿USE AdventureWorks2012;
+GO
+
+/*
+Создайте представление VIEW, отображающее данные из таблиц Production.ProductCategory и Production.ProductSubcategory. 
+Сделайте невозможным просмотр исходного кода представления. 
+Создайте уникальный кластерный индекс в представлении по полям ProductCategoryID, ProductSubcategoryID.
+*/
+
+CREATE VIEW Production.vCategorySubCategory(
+	CategoryID,
+	CategoryName,
+	CategoryRowguid,
+	CategoryModifiedDate,
+	SubcategoryID,
+	SubcategoryName,
+	SubcategoryRowguid,
+	SubcategoryModifiedDate
+)
+WITH SCHEMABINDING,
+ENCRYPTION
+AS
+select
+	category.ProductCategoryID as CategoryID,
+	category.Name as CategoryName,
+	category.rowguid as CategoryRowguid,
+	category.ModifiedDate as CategoryModifiedDate,
+	sub.ProductSubcategoryID as SubcategoryID,
+	sub.Name as SubcategoryName,
+	sub.rowguid as SubcategoryRowguid,
+	sub.ModifiedDate as SubcategoryModifiedDate
+from Production.ProductCategory as category
+inner join Production.ProductSubcategory as sub on sub.ProductCategoryID = category.ProductCategoryID
+
+GO
+
+CREATE UNIQUE CLUSTERED INDEX IX_vCategorySubCategory_ProductCategoryID_ProductSubcategoryID
+ON Production.vCategorySubCategory (CategoryID, SubcategoryID);
+GO
+
+/*
+Создайте три INSTEAD OF триггера для представления на операции INSERT, UPDATE, DELETE. 
+Каждый триггер должен выполнять соответствующие операции в таблицах 
+Production.ProductCategory и Production.ProductSubcategory.
+
+*/
+
+CREATE TRIGGER Production.Trg_vCategorySubCategory_Insert ON Production.vCategorySubCategory
+INSTEAD OF INSERT AS
+BEGIN
+
+	INSERT INTO Production.ProductCategory(
+		Name,
+		rowguid,
+		ModifiedDate)
+	SELECT 
+		Name = ins.CategoryName,
+		rowguid = ins.CategoryRowguid,
+		ModifiedDate = GETDATE()
+	FROM inserted AS ins;
+
+	INSERT INTO Production.ProductSubcategory(
+		ProductCategoryID,
+		Name,
+		rowguid,
+		ModifiedDate)
+	SELECT 
+		ProductCategoryID = category.ProductCategoryID,
+		Name = ins.SubcategoryName,
+		rowguid = NEWID(),
+		ModifiedDate = GETDATE()
+	FROM inserted AS ins
+	JOIN Production.ProductCategory AS category 
+		ON category.rowguid = ins.CategoryRowguid
+END;
+GO
+
+CREATE TRIGGER Production.Trg_vCategorySubCategory_Update ON Production.vCategorySubCategory
+INSTEAD OF UPDATE AS
+BEGIN
+		UPDATE	Production.ProductCategory  SET
+				Name = ins.CategoryName,
+				ModifiedDate = GETDATE()
+		FROM inserted AS ins
+		WHERE ins.CategoryID = Production.ProductCategory.ProductCategoryID
+
+		UPDATE	Production.ProductSubcategory  SET
+				Name = ins.SubcategoryName,
+				ModifiedDate = GETDATE()
+		FROM inserted AS ins
+		WHERE ins.SubcategoryID = Production.ProductSubcategory.ProductSubcategoryID
+END;
+
+GO
+
+CREATE TRIGGER Production.Trg_vCategorySubCategory_Delete ON Production.vCategorySubCategory
+INSTEAD OF DELETE AS
+BEGIN
+		DELETE FROM Production.ProductSubcategory 
+		WHERE ProductSubcategoryID IN (SELECT SubcategoryID FROM deleted)
+
+		DELETE FROM Production.ProductCategory 
+		WHERE ProductCategoryID IN (SELECT CategoryID FROM deleted)
+		AND ProductCategoryID NOT IN (SELECT ProductCategoryID FROM Production.ProductSubcategory)
+END;
+
+GO
+
+/*
+Вставьте новую строку в представление, указав новые данные для ProductCategory и ProductSubcategory. 
+Триггер должен добавить новые строки в таблицы Production.ProductCategory и Production.ProductSubcategory.
+Обновите вставленные строки через представление. Удалите строки.
+*/
+
+INSERT INTO Production.vCategorySubCategory (
+	CategoryName,
+	SubcategoryName,
+	CategoryRowguid
+)
+VALUES ('Electronic devices', 'Apple', NEWID());
+
+UPDATE Production.vCategorySubCategory SET
+	CategoryName = 'Electronic devices 2',
+	SubcategoryName = 'Apple 2'
+WHERE CategoryName = 'Electronic devices';
+
+DELETE FROM Production.vCategorySubCategory
+WHERE CategoryName = 'Electronic devices 2';
